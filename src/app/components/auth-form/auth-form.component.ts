ad0f80d23b2326e2dba536751b3e0f5ad90eaab3
divineLush <alvipath@yahoo.com>
2025-02-01 23:20:10 +0700

    basic auth form

--- src/app/components/auth-form/auth-form.component.ts
+++ src/app/components/auth-form/auth-form.component.ts
@@ -1,11 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ApiService } from '../../services/api.service';
+import { AuthFormData } from '../../interfaces/form-data';
+import { concat, map, Observable, of } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-auth-form',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
+  providers: [ApiService],
   templateUrl: './auth-form.component.html',
   styleUrl: './auth-form.component.css'
 })
 export class AuthFormComponent {
+  private isLoading = false;
 
+  private readonly fb = inject(FormBuilder);
+  private readonly apiService = inject(ApiService);
+
+  public readonly form: FormGroup;
+  public readonly submitDisabled$: Observable<boolean>;
+
+  constructor() {
+    this.form = this.fb.group({
+      email: ['', [Validators.required, Validators.email, Validators.maxLength(320)]],
+      password: ['', [Validators.required, Validators.maxLength(320)]],
+    });
+
+    this.submitDisabled$ = concat(of(true), this.form.statusChanges
+      .pipe(map(status => (status !== 'VALID' || this.isLoading))));
+  }
+
+  onSubmit() {
+    if (this.form.invalid) {
+      return;
+    }
+
+    this.isLoading = true;
+    this.apiService.submitForm(this.form.value as AuthFormData)
+      .subscribe({
+        next: (data) => {
+          console.log(data);
+        },
+        error: (err) => {
+          console.error(err);
+        },
+        complete: () => {
+          this.isLoading = false;
+        }
+      });
+  }
 }
---
+++ src/app/services/api.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from "@angular/core";
+import { AuthFormData } from "../interfaces/form-data";
+import { map, of, timer } from "rxjs";
+
+@Injectable()
+export class ApiService {
+  private checkConnection() {
+    return of(!navigator.onLine)
+      .pipe(map(isOffline => {
+        if (isOffline) {
+          throw new Error('Oops! Connection Error')
+        }
+      }));
+  }
+
+  private hasError() {
+    return Boolean(Math.random() < 0.5);
+  }
+
+  submitForm(data: AuthFormData) {
+    return this.checkConnection().pipe(
+      map(() => timer(2000)),
+      map(() => {
+        if (this.hasError()) {
+          throw new Error('Oops! Something went wrong with your login');
+        }
+
+        return data;
+      }));
+  }
+}
