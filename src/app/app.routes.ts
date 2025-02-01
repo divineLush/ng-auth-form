4a46efbd61dd18c044b1cacd540cfdee27e784cf
divineLush <alvipath@yahoo.com>
2025-02-01 20:40:37 +0700

    init

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { AuthFormComponent } from './components/auth-form/auth-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: AuthFormComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  }
+];
