9cc7893b9ea0d0a6c828eef6ee5ca5e3f7376605
divineLush <alvipath@yahoo.com>
2025-02-02 12:26:31 +0700

    api service cleanup

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,25 +1,27 @@
 import { Injectable } from "@angular/core";
 import { User } from "../interfaces/user";
-import { map, of, timer } from "rxjs";
+import { map, Observable, throwError, timer } from "rxjs";
 
 @Injectable()
 export class ApiService {
   private checkConnection() {
-    return of(!navigator.onLine)
-      .pipe(map(isOffline => {
-        if (isOffline) {
-          throw new Error('Oops! Connection Error')
-        }
-      }));
+    if (!navigator.onLine) {
+      throw new Error('Oops! Connection Error')
+    }
   }
 
   private hasError() {
     return Boolean(Math.random() < 0.5);
   }
 
-  submitForm(data: User) {
-    return this.checkConnection().pipe(
-      map(() => timer(2000)),
+  submitForm(data: User): Observable<User> {
+    try {
+      this.checkConnection();
+    } catch(e) {
+      return throwError(() => e);
+    }
+
+    return timer(3000).pipe(
       map(() => {
         if (this.hasError()) {
           throw new Error('Oops! Something went wrong with your login');
