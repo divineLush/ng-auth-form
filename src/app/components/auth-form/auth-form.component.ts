d4658b480cc2242a0232569bc75df098cb81f431
divineLush <alvipath@yahoo.com>
2025-02-02 12:27:04 +0700

    error messages & auth form cleanup

--- src/app/components/auth-form/auth-form.component.ts
+++ src/app/components/auth-form/auth-form.component.ts
@@ -1,7 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { concat, map, Observable, of } from 'rxjs';
 import { ApiService } from '../../services/api.service';
 import { User } from '../../interfaces/user';
 import { AuthService } from '../../services/auth.service';
@@ -14,23 +13,20 @@ import { AuthService } from '../../services/auth.service';
   styleUrl: './auth-form.component.css'
 })
 export class AuthFormComponent {
-  private isLoading = false;
-
   private readonly fb = inject(FormBuilder);
   private readonly apiService = inject(ApiService);
   private readonly authService = inject(AuthService);
 
   public readonly form: FormGroup;
-  public readonly submitDisabled$: Observable<boolean>;
+
+  public isLoading = false;
+  public error = '';
 
   constructor() {
     this.form = this.fb.group({
       email: ['', [Validators.required, Validators.email, Validators.maxLength(320)]],
       password: ['', [Validators.required, Validators.maxLength(320)]],
     });
-
-    this.submitDisabled$ = concat(of(true), this.form.statusChanges
-      .pipe(map(status => (status !== 'VALID' || this.isLoading))));
   }
 
   onSubmit() {
@@ -41,15 +37,18 @@ export class AuthFormComponent {
     this.isLoading = true;
     this.apiService.submitForm(this.form.value as User)
       .subscribe({
-        next: (data) => {
-          this.authService.signIn(data);
-        },
-        error: (err) => {
-          console.error(err);
-        },
-        complete: () => {
+        next: (data: User) => {
           this.isLoading = false;
-        }
+          try {
+            this.authService.signIn(data);
+          } catch(e) {
+            this.error = (e as Error).message;
+          }
+        },
+        error: (err: Error) => {
+          this.isLoading = false;
+          this.error = err.message;
+        },
       });
   }
 }
